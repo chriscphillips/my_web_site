ddc2c6855fa59d8994cbda749b6ee13e00ffd61d
chriscphillips <cptp50wv@shentel.net>
2018-01-06 12:13:15 -0500

    new site

--- my_app.rb
+++ my_app.rb
@@ -1,1 +1,23 @@
-my_app.rb
+require "sinatra"
+
+get '/' do
+    erb :get_greeting
+end
+
+get '/base' do
+    erb :base
+end
+
+get '/welcome_greeting' do
+    erb :welcome_greeting
+end
+
+get '/woodpens' do
+	erb :woodpens, :local => {:welcome_greeting => welcome_greeting}
+end
+
+post '/woodpens' do
+	woodpens = params[:woodpens]
+	redirect '/woodpens?welcome_greeting='+ welcome_greeting
+end
+
